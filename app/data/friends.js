84a0b51d568df04290c198f250b71f607f0645c3
juney <juhnnie@gmail.com>
2019-09-11 23:46:36 -0700

    e

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,8 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the possible friends.
-// ===============================================================================
-
 
 var friends = [
   {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Estevan",
+    photo: "",
     scores: [
       "5",
       "1",
@@ -22,8 +17,8 @@ var friends = [
     ]
   },
   {
-    name: "Jacob Deming",
-    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    name: "Ryan",
+    photo: "",
     scores: [
       "4",
       "2",
@@ -37,72 +32,6 @@ var friends = [
       "2"
     ]
   },
-  {
-    name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-    scores: [
-      "5",
-      "2",
-      "2",
-      "2",
-      "4",
-      "1",
-      "3",
-      "2",
-      "5",
-      "5"
-    ]
-  },
-  {
-    name: "Louis T. Delia",
-    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    scores: [
-      "3",
-      "3",
-      "4",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2",
-      "2",
-      "3"
-    ]
-  },
-  {
-    name: "Lou Ritter",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-    scores: [
-      "4",
-      "3",
-      "4",
-      "1",
-      "5",
-      "2",
-      "5",
-      "3",
-      "1",
-      "4"
-    ]
-  },
-  {
-    name: "Jordan Biason",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-    scores: [
-      "4",
-      "4",
-      "2",
-      "3",
-      "2",
-      "2",
-      "3",
-      "2",
-      "4",
-      "5"
-    ]
-  }
-
 ];
 
-// Here we export the array. This makes it accessible to other files using require.
 module.exports = friends;
